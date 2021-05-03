d74ce744eba130232629ff3b344bfa37f4ac9a84
Scott Warchal <scott.warchal@gmail.com>
2021-05-03 13:21:41 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="parserix",
-      version="0.1",
+      version="0.2",
       url="http://github.com/swarchal/parserix",
       description="parse metadata from ImageXpress files and paths",
       author="Scott Warchal",
